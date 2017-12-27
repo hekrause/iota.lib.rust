321c017e6ab93541e701cf89a39b2a286d705025
hekrause <hekrause@htwg-konstanz.de>
2017-12-27 17:45:27 +0100

    :pencil: Edit bencher files and travis.

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,7 +5,11 @@ extern crate test;
 extern crate iota_lib_rust;
 
 use test::test::Bencher;
+
+use iota_lib_rust::utils::Signing::*;
 use iota_lib_rust::utils::Converter::*;
+use iota_lib_rust::pow::RCurl::*;
+use iota_lib_rust::pow::TCurl::*;
 use iota_lib_rust::utils::SeedRandomGenerator;
 
 #[bench]
@@ -28,5 +32,17 @@ fn bench_from_trits_to_tryte(b: &mut Bencher) {
     b.iter(|| {
         let trits = vec![1, -1, 0, 1, 1, -1, 1, -1, 0, 1, 1, 0, 0, -1, 0, 1, -1, 1, -1, 0, 1, -1, 0, 1, 0, 0, 0, -1, 0, 1, 1, 1, 0, 0, 0, 0, -1, 1, 1, 0, 0, 1, 0, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 0, 1, 0, -1, 0, 0, -1, 1, 1, -1, 1, -1, 0, 0, -1, 1, 0, -1, 1, 0, -1, 0, 1, 0, 0, -1, 0, -1, -1, 1, 0, 1, -1, -1, 1, -1, 1, 1, -1, -1, 0, 0, 1, 1, -1, 1, 1, 0, 0, 0, -1, 1, 0, -1, -1, 1, 1, -1, -1, 0, 0, -1, -1, -1, 1, 0, -1, 0, -1, -1, 1, 1, -1, -1, -1, 0, -1, -1, 0, -1, -1, 0, 0, 1, 0, 0, 1, 0, -1, 0, 1, -1, 1, 1, -1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 1, 0, -1, 1, 1, -1, -1, 0, 0, 1, -1, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, 0, -1, -1, 0, -1, -1, -1, 1, -1, -1, 1, 1, -1, 0, -1, 1, -1, -1, -1, 1, 0, 0, -1, 0, -1, 1, -1, 0, 1, 1, 1, -1, -1, 1, 1, 0, 1, 1, 1, -1, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, 1, 0, -1, 1, -1, 0, -1, 1, 1, 0, 1, 1, -1, 0];
         let _tryte= from_trits_to_tryte(trits);
+    });
+}
+
+#[bench]
+fn bench_generate_new_address_with_security_18(b: &mut Bencher) {
+    b.iter(|| {
+        let mut signing: Signing<RCurl> = Signing::new(TCurl::new(Mode::CURLP81));
+        let seed = SeedRandomGenerator::generateNewSeed();
+        let key = signing.key(from_tryte_to_trits(seed), 0, 1);
+        let digest = signing.digest(&key);
+        let address_trits = signing.address(&digest);
+        let _address = from_trits_to_tryte(address_trits);
     });
 }
